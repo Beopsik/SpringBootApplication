182cce95e2275d55659d6bd488fafcb9ed5af3d9
BeobSik <tjswl756@naver.com>
2021-07-28 16:55:23 +0900

    feat: Learn about Banner

--- src/main/java/com/example/springbootapplication/Application.java
+++ src/main/java/com/example/springbootapplication/Application.java
@@ -1,5 +1,6 @@
 package com.example.springbootapplication;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app=new SpringApplication(Application.class);
+        app.setBanner((environment, sourceClass, out) -> {
+            out.println("=======================");
+            out.println("Code Banner");
+            out.println("=======================");
+        });
+        //app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
