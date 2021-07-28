50f50813423e82ab2157c01fa316c299d2c6d591
BeobSik <tjswl756@naver.com>
2021-07-28 17:06:27 +0900

    feat: Learn about Banner and Builder

--- src/main/java/com/example/springbootapplication/Application.java
+++ src/main/java/com/example/springbootapplication/Application.java
@@ -3,19 +3,31 @@ package com.example.springbootapplication;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication app=new SpringApplication(Application.class);
+        /*SpringApplication app=new SpringApplication(Application.class);
         app.setBanner((environment, sourceClass, out) -> {
             out.println("=======================");
             out.println("Code Banner");
             out.println("=======================");
         });
         //app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+        app.run(args);*/
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .banner((environment, sourceClass, out) -> {
+                    out.println("=======================");
+                    out.println("Builder Banner");
+                    out.println("=======================");
+                })
+                .run(args);
     }
 
 }
